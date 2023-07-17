a582735ad2d1929ed5b9a0d5c4527812658e6f25
TZhao_1234 <51836654+zhaozhon93@users.noreply.github.com>
2023-07-16 23:19:41 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 
-
+st.image("Bonterra_Logo.jpg")
 st.title("Hello This is the Demo")
 st.subheader("lets test it out")
 st.text('looks good')
-st.image("Bonterra_logo.jpg")
+
 Table = st.file_uploader("please upload an CSV file",type=["csv","xlsx"],accept_multiple_files=True)
 if Table is not None:
     for i in Table:
