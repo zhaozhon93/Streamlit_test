6d9c57685381f63892b6f690e90391601f680bab
TZhao_1234 <51836654+zhaozhon93@users.noreply.github.com>
2023-07-17 11:56:10 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 
 st.image("Bonterra_Logo.jpg")
@@ -6,7 +7,12 @@ st.title("Hello This is the Demo")
 st.subheader("lets test it out")
 st.text('looks good')
 
-Table = st.file_uploader("please upload an CSV file",type=["csv","xlsx"],accept_multiple_files=True)
-if Table is not None:
-    for i in Table:
-        st.table(i)
+data_file = st.file_uploader("please upload an CSV file",type=["csv","xlsx"],accept_multiple_files=True)
+
+if data_file is not None:
+    for i in data_file:
+        
+        file_detail = {"FileName":i.name,"FileType":i.type,"File_ize":i.size}
+        st.write(file_detail)
+        df = pd.read_csv(i)
+        st.dataframe(df)
